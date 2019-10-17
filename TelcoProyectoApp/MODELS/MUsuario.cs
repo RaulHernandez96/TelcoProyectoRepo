17ca572aaa09f93bc81414c9942466ba3f26663d
145515 - HENRY ROBERTO BATEN AMAYA <hbaten0406@gmail.com>
2019-10-17 00:05:35 -0600

    Modelo usuario

---
+++ TelcoProyectoApp/MODELS/MUsuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELS
+{
+    public class MUsuario
+    {
+        public int idUsuario { get; set; }
+        public int idCliente { get; set; }
+        public int idTipoUsuario { get; set; }
+        public int idFinanciera { get; set; }
+        public string usuario { get; set; }
+        public string contrasenia { get; set; }
+        public string deBaja { get; set; }
+    }
+}
