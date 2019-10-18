22d13c0bd713f402e49bb8ea2c89151aad5a1855
kevin velasquez <kevinvelasquezdonis@gmail.com>
2019-10-18 02:19:09 -0600

    creacion de formulario de solicitud de tarjeta de credito y los scrip que mande al grupo falta terminar este form

--- TelcoProyectoApp/TelcoProyectoWebSite/main/solicitud_tarjeta_credito.aspx
+++ TelcoProyectoApp/TelcoProyectoWebSite/main/solicitud_tarjeta_credito.aspx
@@ -1,11 +1,11 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="solicitud_prestamo.aspx.cs" Inherits="main_solicitud_prestamo" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="solicitud_tarjeta_credito.aspx.cs" Inherits="main_solicitud_tarjeta_credito" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Solicitud de prestamo</title>
+    <title>Solicitud de Tarjeta de Credito</title>
 </head>
 <body>
     <form id="form1" runat="server">
@@ -25,29 +25,20 @@
             <asp:Label ID="Label5" runat="server" Text="Tipo desembolso"></asp:Label>
             <asp:TextBox ID="txtTipo_desembolso" runat="server"></asp:TextBox>
             <br />
-            <asp:Label ID="Label6" runat="server" Text="capital"></asp:Label>
-            <asp:TextBox ID="txtCapital" runat="server"></asp:TextBox>
-            <br />
-            <asp:Label ID="Label7" runat="server" Text="plazo"></asp:Label>
-            <asp:TextBox ID="txtPlazo" runat="server"></asp:TextBox>
-            <br />
-            <asp:Label ID="Label8" runat="server" Text="Garantia"></asp:Label>
-            <asp:TextBox ID="txtGarantia" runat="server"></asp:TextBox>
-            <br />
-            <asp:Label ID="Label9" runat="server" Text="utilidad de dinero"></asp:Label>
-            <asp:TextBox ID="txtUtilidad_dinero" runat="server"></asp:TextBox>
+            <asp:Label ID="Label6" runat="server" Text="Limite"></asp:Label>
+            <asp:TextBox ID="txtlimite" runat="server"></asp:TextBox>
             <br />
             <asp:Label ID="Label10" runat="server" Text="Historial de compra"></asp:Label>
             <asp:TextBox ID="txtHistorial_compras" runat="server"></asp:TextBox>
-            <br />
-            <asp:Label ID="Label11" runat="server" Text="proyeccion de pago"></asp:Label>
-            <asp:TextBox ID="txtProyeccion" runat="server"></asp:TextBox>
             <br />
             <asp:Label ID="Label12" runat="server" Text="Estado de Autorizacion"></asp:Label>
             <asp:TextBox ID="txtEstado_autorizacion" runat="server"></asp:TextBox>
             <br />
             <asp:Label ID="Label13" runat="server" Text="fecha de creacion"></asp:Label>
             <asp:TextBox ID="txtFecha" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label7" runat="server" Text="categoria de la tarjeta "></asp:Label>
+            <asp:TextBox ID="txtCategoria_tarjeta" runat="server"></asp:TextBox>
             <br />
             <asp:Button ID="btnInsertar" runat="server" Text="Crear Solicitud" OnClick="btnInsertar_Click" />
         </div>
--- TelcoProyectoApp/TelcoProyectoWebSite/main/solicitud_tarjeta_credito.aspx.cs
+++ TelcoProyectoApp/TelcoProyectoWebSite/main/solicitud_tarjeta_credito.aspx.cs
@@ -5,9 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
-using System.Windows.Input;
 
-public partial class main_solicitud_prestamo : System.Web.UI.Page
+public partial class main_solicitud_tarjeta_credito : System.Web.UI.Page
 {
     SqlConnection conn = new SqlConnection(@"data source=(local)\sqle2017,Initial Catalog=financiera; Integrated Security=true");
     protected void Page_Load(object sender, EventArgs e)
@@ -21,21 +20,21 @@ public partial class main_solicitud_prestamo : System.Web.UI.Page
             conn.Open();
         SqlCommand sqlcomm = new SqlCommand("[dbo].[PRC_CREA_SOLICITUD_CREDITO]", conn);
         sqlcomm.CommandType = System.Data.CommandType.StoredProcedure;
-        sqlcomm.Parameters.AddWithValue("@pIdSolicitud",txtid_financiera.Text.Trim());
         sqlcomm.Parameters.AddWithValue("@pIdSolicitud", txtid_financiera.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pIdCliente",txtDPI_cliente.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pIdTipoCredito",txtTipo_credito.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pIdTipoEntrega",txtTipo_entrega.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pIdTipoDesembolso",txtTipo_desembolso.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pCapital",txtCapital.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pPlazo",txtPlazo.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pGarantia",txtGarantia.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pUtilidadDinero",txtUtilidad_dinero.Text.Trim());
-        sqlcomm.Parameters.AddWithValue("@pHistorialCompra",txtHistorial_compras.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pIdSolicitud", txtid_financiera.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pIdCliente", txtDPI_cliente.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pIdTipoCredito", txtTipo_credito.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pIdTipoEntrega", txtTipo_entrega.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pIdTipoDesembolso", txtTipo_desembolso.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pCapital", txtCapital.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pPlazo", txtPlazo.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pGarantia", txtGarantia.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pUtilidadDinero", txtUtilidad_dinero.Text.Trim());
+        sqlcomm.Parameters.AddWithValue("@pHistorialCompra", txtHistorial_compras.Text.Trim());
         sqlcomm.Parameters.AddWithValue("@pProyeccionPago", txtProyeccion.Text.Trim());
         sqlcomm.ExecuteNonQuery();
         conn.Close();
-        if(int.Parse(txtid_financiera.Text.Trim())==0)
+        if (int.Parse(txtid_financiera.Text.Trim()) == 0)
         {
             string script = "alert(\"Solicitud Creada correctamente\");";
             ScriptManager.RegisterStartupScript(this, GetType(),
@@ -48,7 +47,6 @@ public partial class main_solicitud_prestamo : System.Web.UI.Page
             ScriptManager.RegisterStartupScript(this, GetType(),
                                   "ServerControlScript", script, true);
         }
-
 
     }
 }
