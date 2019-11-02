e517ce85de7e0607590aceaf4ad7825b5767e8cd
145515 - HENRY ROBERTO BATEN AMAYA <hbaten0406@gmail.com>
2019-11-01 23:30:09 -0600

    commit login

--- TelcoProyectoApp/TelcoProyectoWebSite/security/InicioSesion.aspx.cs
+++ TelcoProyectoApp/TelcoProyectoWebSite/security/InicioSesion.aspx.cs
@@ -29,7 +29,6 @@ public partial class security_Default : System.Web.UI.Page
                 Session["atributos_usuario"] = objBLLLogin.atributosUsuario(Convert.ToInt32(dt.Rows[0]["id_usuario"].ToString()));
                 Session["permisos_forma"] = dt;
                 Response.Redirect(ResolveUrl("~/" + "Default.aspx"));
-
                 
             }
 
