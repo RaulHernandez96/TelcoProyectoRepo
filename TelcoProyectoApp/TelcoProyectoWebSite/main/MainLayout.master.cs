9c5c8e0d3fa44150368793ae8c714a1e636bbd58
kevin velasquez <kevinvelasquezdonis@gmail.com>
2019-11-02 02:14:09 -0600

    a ver que pex con esta madre

--- TelcoProyectoApp/TelcoProyectoWebSite/main/MainLayout.master.cs
+++ TelcoProyectoApp/TelcoProyectoWebSite/main/MainLayout.master.cs
@@ -12,11 +12,11 @@ public partial class main_MainLayout : System.Web.UI.MasterPage
   {
         DataTable dt = (DataTable)Session["permisos_forma"];
 
-     /*   foreach (DataRow dtRow in dt.Rows)
+        foreach (DataRow dtRow in dt.Rows)
         {
 
             literalMenu.Text += "<li><a href = "+ ResolveUrl("~/" + dtRow["nombre_forma"].ToString() )+ "><i class='fa fa-calendar'></i><span>" + dtRow["descripcion"] + "</span><span class='pull-right-container'><small class='label pull-right bg-red'>3</small><small class='label pull-right bg-blue'>17</small></span></a></li>";
         }
-    */
+    
   }
 }
